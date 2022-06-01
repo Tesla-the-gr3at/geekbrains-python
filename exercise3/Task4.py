0c1529d5f74f343565bd0f16a9ecca987dc16a51
konstantin.gromov <konstantin.gromov@itiviti.com>
2022-06-01 11:37:03 +0200

    Works

--- exercise3/Task4.py
+++ exercise3/Task4.py
@@ -1,4 +1,6 @@
 def my_func(a,b):
+
+    # Do not use built-in function:
     if (not a >=0) or (not b <=0):
         print("First number must be more than 0, scond number must be less than 0")
         exit(0)
@@ -7,8 +9,17 @@ def my_func(a,b):
         exit(0)
 
     b_module = b * -1
+    result = a
 
-    for i in range(1,b_module+1):
-        pass # broken
-print(my_func(5,-2))
+    for i in range(1,b_module):
+        result = result * a
+    
+    result = 1 / result
+    
+
+    #use built-in function
+    result2 = 1 / (a**b_module)
+    
+    return result, result2
+print(my_func(4,-4))
     
