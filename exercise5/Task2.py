9eb010b96ede50481f5a2a5c75367dd6a8b627ac
Konstantin Gromov <konstantin.gromov@itiviti.com>
2022-07-21 11:36:44 +0200

    Remove intendation

--- exercise5/Task2.py
+++ exercise5/Task2.py
@@ -4,15 +4,15 @@ import sys
 
 with open(os.path.join(sys.path[0], "task2.txt"), 'r') as file:
     lines = file.readlines()
-    print(f"File has {len(lines)} lines")                       # Count number of lines
-    line_number = 0
+print(f"File has {len(lines)} lines")                       # Count number of lines
+line_number = 0
 
-    for line in lines:                                          # Go through each line
-        line_number += 1                                        # Go to next line
-        if len(line.strip()) == 0:                              # Remove spaces and check if the line is empty
-            print(f"Line {line_number} has 0 words.")           # Print number of words
-            
-        else:                                                   # If the line is not empty
-            line = line.split(" ")                              # Convert lne to a list
-            print(f"Line {line_number} has {len(line)} words")  # Print number of words
+for line in lines:                                          # Go through each line
+    line_number += 1                                        # Go to next line
+    if len(line.strip()) == 0:                              # Remove spaces and check if the line is empty
+        print(f"Line {line_number} has 0 words.")           # Print number of words
         
+    else:                                                   # If the line is not empty
+        line = line.split(" ")                              # Convert lne to a list
+        print(f"Line {line_number} has {len(line)} words")  # Print number of words
+    
